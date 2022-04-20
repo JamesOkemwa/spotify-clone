867a57ed570467148619940198c0ab1b2d2dd40d
lejames <okejames55@gmail.com>
2022-04-20 17:56:41 +0300

    Render songs from the individual playlists

--- components/Center.js
+++ components/Center.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/outline';
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { shuffle } from "lodash"
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { playlistIdState, playlistState } from '../atoms/playlistAtom'
@@ -35,10 +35,13 @@ function Center() {
     }, [spotifyApi, playlistId])
     
     return (
-        <div className="flex-grow text-white">
+        <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
-                <div className="flex items-center bg-black space-x-3 
-                opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+                <div 
+                    className="flex items-center bg-black space-x-3 
+                        opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white"
+                    onClick={signOut}
+                >
                     <img 
                         className="rounded-full w-10 h-10"
                         src={session?.user.image} 
